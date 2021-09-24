4a46751505dc8577bed210adc597c6696708e21f
Gianmarco Pettinato <gianmarco@pettinato.eu>
2021-09-24 16:19:56 +0200

    first stable release

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { setupApis } from './api';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 setupApis(app);
